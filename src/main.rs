adb73a9269bda80c6795c5e2a36d5f6aee720670
Chris Allen <58678379+Skelt3r@users.noreply.github.com>
2022-06-08 10:25:17 -0500

    Add help and exit commands

--- src/main.rs
+++ src/main.rs
@@ -35,42 +35,68 @@ fn print_multi_roll(rolls: (Vec<i32>, i32, i32)) {
     println!("Sum: {}", rolls.2)
 }
 
-// Process the user's input string and return usable elements
-fn get_user_input() -> Vec<i32> {
+// Print help info
+fn print_help() {
+    println!("----------------------------------------------------");
+    println!("\nRoll dice with any of the following command formats:\nxdx\nxdx+x\nxdx-x ");
+    println!("\nExamples:\n1d20\n2d8+5\n4d6-1");
+    println!("\nType \"exit\" or \"quit\" to terminate the program.");
+    println!("----------------------------------------------------");
+}
+
+// Print error message and restart the main loop
+fn print_error() -> i32 {
+    println!("\nBad input, try again.");
+    run();
+    1
+}
+
+// Get user input from the command line
+fn get_user_input() -> String {
     let mut command = String::new();
-    let mut nums: Vec<i32> = Vec::new();
-
     println!("\nRoll the dice:");
-
     io::stdin().read_line(&mut command).expect("Failed to read input...");
+    command
+}
 
-    let numstrings: Vec<&str> = command.split(&['d', '-', '+'][..]).collect();
-    let input: Vec<&str> = command.split_inclusive(&['d', '-', '+'][..]).collect();
+// Process the user's input string and return usable elements
+fn process_user_input(command: String) -> Vec<i32> {
+    let mut nums: Vec<i32> = Vec::new();
+    let trim = command.trim();
 
-    for n in numstrings {
-        nums.push(match n.trim().parse() {
-            Ok(num) => num,
-            Err(_) => quit::with_code(0),
-        });
-    }
-
-    let i1: Vec<char> = input[1].chars().collect();
-
-    if i1.contains(&'-') {
-        nums.splice(2..3, [-nums[2]]);
-    }
-
-    if nums.len() < 3 {
-        nums.push(0);
+    if trim == "help" {
+        print_help();
+    } else if trim == "quit" || trim == "exit" {
+        quit::with_code(0);
+    } else {
+        let numstrings: Vec<&str> = command.split(&['d', '-', '+'][..]).collect();
+        let input: Vec<&str> = command.split_inclusive(&['d', '-', '+'][..]).collect();
+    
+        for n in numstrings {
+            nums.push(match n.trim().parse() {
+                Ok(num) => num,
+                Err(_) => print_error(),
+            });
+        }
+    
+        let i1: Vec<char> = input[1].chars().collect();
+    
+        if i1.contains(&'-') {
+            nums.splice(2..3, [-nums[2]]);
+        }
+    
+        if nums.len() < 3 {
+            nums.push(0);
+        }
     }
 
     nums
 }
 
-#[quit::main]
-fn main() {
+fn run() {
     loop {
-        let nums = get_user_input();
+        let command = get_user_input();
+        let nums = process_user_input(command);
 
         if nums.len() > 1 {
             if nums[0] == 1 {
@@ -80,4 +106,9 @@ fn main() {
             }
         }
     }
+}
+
+#[quit::main]
+fn main() {
+    run()
 }
